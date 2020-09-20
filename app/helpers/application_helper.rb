ed2482e01713f6044950f38fcbd753b4d744c701
Jerico Luis Lua <jericoluislua@yahoo.com.ph>
2020-09-20 06:42:06 -0700

    Navigation bar more or else complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #method I used for svgs can be found in https://coderwall.com/p/d1vplg/embedding-and-styling-inline-svg-documents-with-css-in-rails
+  def embedded_svg filename, options={}
+    file = File.read(Rails.root.join('app', 'assets', 'images', 'svg', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
 end
