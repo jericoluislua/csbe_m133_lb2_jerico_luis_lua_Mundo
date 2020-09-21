bcdcfe2b0f81bce76f846a7b3283a77305e21847
Jerico Luis Lua <jericoluislua@yahoo.com.ph>
2020-09-21 09:11:40 -0700

    Added gem devise. Added discography, about, contact, sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new', as: 'login_form'
+  post 'sessions', to: 'sessions#create', as: 'sign_in_method'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'contact', to: 'contact#index', as: 'contact'
+  get 'about', to: 'about#index', as: 'about'
+  get 'discography', to: 'discography#index', as: 'discography'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
