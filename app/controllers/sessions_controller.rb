3bfece6cff3b82ef0d7a0fe7c119402782a5debb
Jerico Luis Lua <jericoluislua@yahoo.com.ph>
2020-10-02 06:16:55 -0700

    Sign in, Register and Logout now works. Navigation links not secured yet. Profile also created.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,40 @@
 class SessionsController < ApplicationController
-  #SessionsController idea from https://www.youtube.com/watch?v=FwfsMv2kSX4
   def new
   end
 
   def create
     #Finds the user email through the user.email params and checks if the password matches to the email
-    @user = User.find_by(email: params["user"]["email"])
-        .try(:authenticate, params["user"]["password"])
-    if @user
-      session[:user_id] = @user.id
+    @user = User.find_by("email = ?", user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      session[:id] = @user.id
+      redirect_to action: "profile"
+    else
+      flash[:controller_error] = "Email or password is invalid"
+      flash[:error] = @user.errors.full_messages
+      render "login"
+    end
+  end
+
+  def profile
+    if session[:id]
+    else
+      if !session[:id]
+        redirect_to(login_form_path)
+      end
     end
   end
 
   def destroy
+    if session[:id]
+      reset_session
+      redirect_to(login_form_path)
+    else
+      redirect_to(login_form_path)
+    end
+
+  end
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'register', to: 'users#new', as: 'new_user'
   post 'users/create', to: 'users#create', as: 'create_new_user'
+  get 'profile', to: 'sessions#profile', as: 'profile'
   get 'login', to: 'sessions#new', as: 'login_form'
-  post 'sessions', to: 'sessions#create', as: 'sign_in_method'
-  delete 'logout', to: 'sessions#destroy', as: 'logout'
+  post 'sessions/create', to: 'sessions#create', as: 'login_method'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'contact', to: 'contact#index', as: 'contact'
   get 'about', to: 'about#index', as: 'about'
   get 'discography', to: 'discography#index', as: 'discography'
